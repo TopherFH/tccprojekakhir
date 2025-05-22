0a3299bb12e1def369ba7d967fd1bfd02abeb07c
Sakti Maulana <reskyfebrianto170@gmail.com>
2025-05-22 19:59:43 +0700

    frontend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://127.0.0.1:5500",
     credentials: true,
 }));
 app.use(cookieParser());
