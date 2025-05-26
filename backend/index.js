230adbb1721b64717b5916c7491ad0d7ce438970
Kristopher Fredrik <kristoperfh@gmail.com>
2025-05-26 10:51:36 +0700

    index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "http://localhost:5500",
     credentials: true,
 }));
 app.use(cookieParser());
